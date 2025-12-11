65de0397b554bdf95341e8cc37f017d92093de2e
DevHero <register@ireallyhost.com>
2025-12-12 02:01:52 +0700

    Update style

--- twofactor_gauthenticator_form.js
+++ twofactor_gauthenticator_form.js
@@ -19,14 +19,15 @@ if (window.rcmail) {
 	//twofactor input form
     var text = '';
     text += '<tr>';
-    text += '<td class="title"><label for="2FA_code">'+rcmail.gettext('two_step_verification_form', 'twofactor_gauthenticator')+'</label></td>';
-    text += '<td class="input"><input name="_code_2FA" id="2FA_code" size="10" autocapitalize="off" autocomplete="off" type="' + twoFactorCodeFieldType + '" maxlength="10"></td>';
+    text += '<td class="title"><label for="2FA_code" class="mt-2 font-weight-bold">'+rcmail.gettext('two_step_verification_form', 'twofactor_gauthenticator')+'</label></td>';
+    text += '<td class="input"><input name="_code_2FA" id="2FA_code" size="10" autocapitalize="off" autocomplete="off" type="' + twoFactorCodeFieldType + '" maxlength="10" class="form-control"></td>';
     text += '</tr>';
 
     // remember option
     if(rcmail.env.allow_save_device_30days){
 		text += '<tr>';
-		text += '<td class="title" colspan="2"><input type="checkbox" id="remember_2FA" name="_remember_2FA" value="yes"/><label for="remember_2FA">'+rcmail.gettext('dont_ask_me_30days', 'twofactor_gauthenticator')+'</label></td>';
+		text += '<td class="title py-2" colspan="2"><div class="form-check">
+			<input type="checkbox" id="remember_2FA" name="_remember_2FA" value="yes" class="form-check-input"/><label for="remember_2FA" class="form-check-label">'+rcmail.gettext('dont_ask_me_30days', 'twofactor_gauthenticator')+'</label></div></td>';
 		text += '</tr>';
 	}
 
